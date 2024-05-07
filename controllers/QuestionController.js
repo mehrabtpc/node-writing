9ad9fde12644928b5e025a69a19dcfc6277dbd19
mehrab <mehrabt.pc@gmail.com>
2024-05-07 10:25:02 +0330

    Modify show question

--- controllers/QuestionController.js
+++ controllers/QuestionController.js
@@ -13,6 +13,9 @@ const getQuestions = async (req, res) => {
     try{
       const { id } = req.params;
       const question = await Question.findById(id);
+      if (!question) {
+        return res.status(404).json({ message: "Question not found" });
+      }
       res.status(200).json(question);
     }catch(error){
       res.status(500).json({ message: error.message });
